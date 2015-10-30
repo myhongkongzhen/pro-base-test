2b34814745eb5937d9ac1a124bd770a2fda6b0ac
Z_Z.W <myhongkongzhen@gmail.com>
2015-10-30 16:56:51 +0800

    根據findbugs插件優化部分代碼

--- src/main/java/z/z/w/test/aio/AioClient.java
+++ src/main/java/z/z/w/test/aio/AioClient.java
@@ -12,20 +12,20 @@ import java.util.concurrent.ExecutionException;
 /**************************************************************************
  * <pre>
  *     FileName: z.z.w.test.aio.AioClient.java
- *         Desc: 
+ *         Desc:
  *       author: Z_Z.W - myhongkongzhen@gmail.com
  *      version: 2015年9月21日 上午11:57:10
- *   LastChange: 2015年9月21日 上午11:57:10 
+ *   LastChange: 2015年9月21日 上午11:57:10
  *      History:
  * </pre>
  **************************************************************************/
 public class AioClient
 {
 	private final static Logger logger = LoggerFactory.getLogger( AioClient.class );
-	
+
 	/**
 	 * Create by : 2015年9月21日 上午11:57:10
-	 * 
+	 *
 	 * @param args
 	 */
 	public static void main( String[] args )
@@ -33,13 +33,13 @@ public class AioClient
 		try
 		{
 			AsynchronousSocketChannel client = AsynchronousSocketChannel.open();
-			client.connect( new InetSocketAddress( "127.0.0.1" , 8888 ) );
+			client.connect( new InetSocketAddress( "127.0.0.1", 8888 ) );
 			client.write( ByteBuffer.wrap( "你好jflkdsfjlskdjflasjfksajlfdsjlkfjsdlkfjlsakd".getBytes() ) ).get();
 		}
 		catch ( IOException | InterruptedException | ExecutionException e )
 		{
-			logger.error( "IOException | InterruptedException | ExecutionException error : {}." , e.toString() );
-			logger.error( "AioClient error : {}." , e.getMessage() , e );
+			logger.error( "IOException | InterruptedException | ExecutionException error : {}.", e.toString() );
+			logger.error( "AioClient error : {}.", e.getMessage(), e );
 		}
 	}
 }
--- src/main/java/z/z/w/test/aio/AioServer.java
+++ src/main/java/z/z/w/test/aio/AioServer.java
@@ -1,5 +1,8 @@
 package z.z.w.test.aio;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -7,10 +10,10 @@ import java.nio.channels.AsynchronousChannelGroup;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
-import java.util.concurrent.*;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**************************************************************************
  * <pre>
@@ -19,12 +22,12 @@ import org.slf4j.LoggerFactory;
  **************************************************************************/
 public class AioServer
 {
-	public final static int					PORT		= 8888;
-	private final static Logger				logger		= LoggerFactory.getLogger( AioServer.class );
-	private final ExecutorService			executor	= Executors.newFixedThreadPool( 20 );
-	private AsynchronousChannelGroup		asyncChannelGroup;											// aio的核心之一通道组.由它负责处理事件,完成之后通知相应的handler
-	private AsynchronousServerSocketChannel	server		= null;											// 端口侦听器
-	
+	public final static  int             PORT     = 8888;
+	private final static Logger          logger   = LoggerFactory.getLogger( AioServer.class );
+	private final        ExecutorService executor = Executors.newFixedThreadPool( 20 );
+	private AsynchronousChannelGroup asyncChannelGroup;                                            // aio的核心之一通道组.由它负责处理事件,完成之后通知相应的handler
+	private AsynchronousServerSocketChannel server = null;                                            // 端口侦听器
+
 	public AioServer()
 	{
 		try
@@ -34,13 +37,13 @@ public class AioServer
 		}
 		catch ( IOException e )
 		{
-			logger.error( "Aioserver construction error : {}." , e.getMessage() , e );
+			logger.error( "Aioserver construction error : {}.", e.getMessage(), e );
 		}
 	}
-	
+
 	/**
 	 * Create by : 2015年9月21日 上午11:32:35
-	 * 
+	 *
 	 * @param args
 	 */
 	public static void main( String[] args )
@@ -48,58 +51,57 @@ public class AioServer
 		new AioServer().completionHandler();
 	}
 
-
 	public void completionHandler()
 	{
-		logger.info( "Server listen on {}. " , PORT );
+		logger.info( "Server listen on {}. ", PORT );
 		// 注册事件和事件完成后的处理器 ,accepte()完成异步注册后，线程就可以继续自己的处理了，完全不被这个io所中断
-		server.accept( null , new CompletionHandler< AsynchronousSocketChannel, Object >()
+		server.accept( null, new CompletionHandler<AsynchronousSocketChannel, Object>()
 		{
 			final ByteBuffer buffer = ByteBuffer.allocate( 1024 );
-			
-			public void completed( AsynchronousSocketChannel result , Object attachment )
+
+			public void completed( AsynchronousSocketChannel result, Object attachment )
 			{
-				logger.info( "start thread : {}." , Thread.currentThread().getName() );
+				logger.info( "start thread : {}.", Thread.currentThread().getName() );
 				try
 				{
 					buffer.clear();
-					result.read( buffer ).get( 100 , TimeUnit.SECONDS );
+					result.read( buffer ).get( 100, TimeUnit.SECONDS );
 					buffer.flip();
-					logger.info( "received message: {}" , new String( buffer.array() ) );
+					logger.info( "received message: {}", new String( buffer.array(), "utf-8" ) );
 				}
 				catch ( InterruptedException | ExecutionException e )
 				{
-					logger.error( "InterruptedException | ExecutionException error : {}." , e.toString() );
-					logger.error( "CompletionHandler error : {}." , e.getMessage() , e );
+					logger.error( "InterruptedException | ExecutionException error : {}.", e.toString() );
+					logger.error( "CompletionHandler error : {}.", e.getMessage(), e );
 				}
-				catch ( TimeoutException e )
+				catch ( Exception e )
 				{
-					logger.error( "CompletionHandler error : {}." , e.getMessage() , e );
+					logger.error( "CompletionHandler error : {}.", e.getMessage(), e );
 				}
 				finally
 				{
-					
+
 					try
 					{
 						result.close();
-						server.accept( null , this );
+						server.accept( null, this );
 					}
 					catch ( Exception e )
 					{
-						logger.error( "CompletionHandler error : {}." , e.getMessage() , e );
+						logger.error( "CompletionHandler error : {}.", e.getMessage(), e );
 					}
 				}
-				
+
 				logger.info( "end" );
 			}
-			
-			public void failed( Throwable exc , Object attachment )
+
+			public void failed( Throwable exc, Object attachment )
 			{
 				logger.info( "failed: " + exc );
 			}
-			
+
 		} );
-		
+
 		// 主线程继续自己的行为
 		while ( true )
 		{
@@ -108,7 +110,8 @@ public class AioServer
 				Thread.sleep( 1000 );
 			}
 			catch ( InterruptedException e )
-			{}
+			{
+			}
 		}
 	}
 }
