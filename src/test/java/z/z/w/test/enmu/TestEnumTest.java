c30959fff0daadd733b98f7f08a4f210dbdd5ed2
Z_Z.W <myhongkongzhen@gmail.com>
2015-11-12 17:57:52 +0800

    添加枚舉測試代碼

---
+++ src/test/java/z/z/w/test/enmu/TestEnumTest.java
@@ -0,0 +1,31 @@
+/**********************************************************************************************************************
+ * Copyright (c) 2015. Lorem ipsum dolor sit amet, consectetur adipiscing elit.                                       *
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.                        *
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.                                                   *
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.                     *
+ * Vestibulum commodo. Ut rhoncus gravida arcu.                                                                       *
+ **********************************************************************************************************************/
+
+package z.z.w.test.enmu;
+
+import org.junit.Test;
+
+/*********************************************************************************************
+ * <pre>
+ *     FileName: z.z.w.test.enmu.TestEnumTest
+ *         Desc:
+ *       author: Z_Z.W - myhongkongzhen@gmail.com
+ *      version: 2015-11-12 16:10
+ *   LastChange: 2015-11-12 16:10
+ *      History:
+ * </pre>
+ *********************************************************************************************/
+public class TestEnumTest
+{
+
+	@Test
+	public void testGetTest() throws Exception
+	{
+		System.out.println( TestEnum.INSTANCE.getTest() );
+	}
+}
